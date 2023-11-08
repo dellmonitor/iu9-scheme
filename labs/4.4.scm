d9ac0fdf707ce908a87485f618d58c5815a776bd
dellmonitor <123smorodina@gmail.com>
2023-11-08 22:19:55 +0300

    fix 4.4 force

--- labs/4.4.scm
+++ labs/4.4.scm
@@ -5,11 +5,12 @@
     ((my-if predicate consequent alternative)
      (let* ((then-expr (delay consequent))
 	    (else-expr (delay alternative)))
-       (or (and predicate (force then-expr))
-	   (force else-expr))))))
+       (force (or (and predicate then-expr)
+	   else-expr))))))
 
 (define the-tests
   (list (test (my-if #t 1 (/ 1 0)) 1)
 	(test (my-if #f (/ 1 0) 1) 1)))
 
 (run-tests the-tests)
+ 
